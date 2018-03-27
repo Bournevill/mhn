238b9cd07e10b5eb9ee059996605a75049a6d308
鲍智博 <baozhibo@gmail.com>
2018-03-27 14:33:08 +0800

    no message

--- scripts/deploy_cowrie.sh
+++ scripts/deploy_cowrie.sh
@@ -30,43 +30,47 @@ apt-get -y install python-dev git supervisor authbind openssl python-virtualenv 
 pip install -U supervisor
 /etc/init.d/supervisor start || true
 
-sed -i 's/Port 22$/Port 2222/g' /etc/ssh/sshd_config
+sed -i 's/Port 22$/Port 22222/g' /etc/ssh/sshd_config
 service ssh restart
 useradd -d /home/cowrie -s /bin/bash -m cowrie -g users
 
 cd /opt
-git clone https://github.com/micheloosterhof/cowrie.git cowrie
+git clone https://github.com/Bournevill/cowrie.git cowrie
 cd cowrie
-virtualenv env
-source env/bin/activate
+virtualenv cowrie-env
+source cowrie-env/bin/activate
 # without the following, i get this error:
 # Could not find a version that satisfies the requirement csirtgsdk (from -r requirements.txt (line 10)) (from versions: 0.0.0a5, 0.0.0a6, 0.0.0a5.linux-x86_64, 0.0.0a6.linux-x86_64, 0.0.0a3)
+pip install --upgrade setuptools
+pip install --upgrade pip
 pip install csirtgsdk==0.0.0a6
 pip install -r requirements.txt 
+pip install -U cryptography
 
 cp cowrie.cfg.dist cowrie.cfg
 sed -i 's/hostname = svr04/hostname = server/g' cowrie.cfg
 sed -i 's/#listen_port = 2222/listen_port = 22/g' cowrie.cfg
-sed -i 's/ssh_version_string = SSH-2.0-OpenSSH_6.0p1 Debian-4+deb7u2/ssh_version_string = SSH-2.0-OpenSSH_6.7p1 Ubuntu-5ubuntu1.3/g' cowrie.cfg
+sed -i 's/ssh_version_string = SSH-2.0-OpenSSH_6.0p1 Debian-4+deb7u2/ssh_version_string = SSH-2.0-OpenSSH_6.6p1 Ubuntu-2ubuntu2.6/g' cowrie.cfg
 sed -i 's/#\[output_hpfeeds\]/[output_hpfeeds]/g' cowrie.cfg
 sed -i "s/#server = hpfeeds.mysite.org/server = $HPF_HOST/g" cowrie.cfg
 sed -i "s/#port = 10000/port = $HPF_PORT/g" cowrie.cfg
 sed -i "s/#identifier = abc123/identifier = $HPF_IDENT/g" cowrie.cfg
 sed -i "s/#secret = secret/secret = $HPF_SECRET/g" cowrie.cfg
 sed -i 's/#debug=false/debug=false/' cowrie.cfg
+sed -i "s/listen_endpoints = tcp:2222:interface=0.0.0.0/listen_endpoints = tcp:22:interface=0.0.0.0/g" cowrie.cfg
 
 chown -R cowrie:users /opt/cowrie/
 touch /etc/authbind/byport/22
 chown cowrie /etc/authbind/byport/22
 chmod 770 /etc/authbind/byport/22
 
-sed -i 's/AUTHBIND_ENABLED=no/AUTHBIND_ENABLED=yes/' start.sh
-sed -i 's/DAEMONIZE=""/DAEMONIZE="-n"/' start.sh
+sed -i 's/AUTHBIND_ENABLED=no/AUTHBIND_ENABLED=yes/' /opt/cowrie/bin/cowrie
+sed -i 's/DAEMONIZE=""/DAEMONIZE="-n"/' /opt/cowrie/bin/cowrie
 
 # Config for supervisor
 cat > /etc/supervisor/conf.d/cowrie.conf <<EOF
-[program:cowrie]
-command=/opt/cowrie/start.sh env
+[program:cowrie]            
+command=/opt/cowrie/bin/cowrie start
 directory=/opt/cowrie
 stdout_logfile=/opt/cowrie/log/cowrie.out
 stderr_logfile=/opt/cowrie/log/cowrie.err
